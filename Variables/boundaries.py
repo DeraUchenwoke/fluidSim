7f2a3bf372261b6f878d406d38cf58c415c3a8e0
Dera Uchenwoke <uchidera1@gmail.com>
2021-08-13 20:16:43 +0100

    Added mesh grid component + added more boundary conditions.

--- Variables/boundaries.py
+++ Variables/boundaries.py
@@ -1,4 +1,17 @@
-# Setting boundary conditions.
+# Cartesian coordinates. 
 # Temperature of water @ 20 degrees Celcius.
+# t = Turbulent.
+# ls = Length scale.
 
-dynamic_viscosity = 1.0016; 
+dynamic_viscosity = 1.0016
+Ux_0 = 0 # Initial mean velocity in the x-direction.
+Uy_0 = 0 # Initial mean velocity in the y-direction.
+
+
+t_intensity = 0 
+t_ls = 0
+
+
+
+
+
--- fluid-sim.py
+++ fluid-sim.py
@@ -1,16 +1,4 @@
 # Test push for 2D Fluid Simulation.
 # Test graph visulisation in VS. 
 
-from math import radians
-import numpy as np     # installed with matplotlib
-import matplotlib.pyplot as plt
-import sys
-
-def main():
-
-    x = np.arange(0, radians(1800), radians(12))
-    plt.plot(x, np.cos(x), 'b')
-    plt.show()
-
-main()
 
