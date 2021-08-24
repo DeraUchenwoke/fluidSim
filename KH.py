926125ac26c03d442b85db5111bc4cbd9aff2a35
Dera Uchenwoke <uchidera1@gmail.com>
2021-08-24 13:44:13 +0100

    Function to calculate the slope graident betweeen neighbouring grid cells.

--- KH.py
+++ KH.py
@@ -1,5 +1,7 @@
-def getConserved (rho, vx, vy, P, gamma, vol):
+import matplotlib.pyplot as plt 
+import numpy as np
 
+def getConservative (rho, vx, vy, P, gamma, vol):
     """
       rho      is matrix of cell densities
       vx       is matrix of cell x-velocity
@@ -12,7 +14,6 @@ def getConserved (rho, vx, vy, P, gamma, vol):
       Momy     is matrix of y-momentum in cells
       Energy   is matrix of energy in cells
     """
-
     Mass = rho * vol
     Momx = Mass * vx 
     Momy = Mass * vy 
@@ -24,25 +25,34 @@ def getConserved (rho, vx, vy, P, gamma, vol):
 
 def getPrimitive (Mass, Momx, Momy, Energy, gamma, vol):
 
-    """
-      Mass     is matrix of mass in cells
-      Momx     is matrix of x-momentum in cells
-      Momy     is matrix of y-momentum in cells
-      Energy   is matrix of energy in cells
-      gamma    is ideal gas gamma
-      vol      is cell volume
-      rho      is matrix of cell densities
-      vx       is matrix of cell x-velocity
-      vy       is matrix of cell y-velocity
-      P        is matrix of cell pressures
-    """
-
     rho = Mass / vol
     vx = Momx / (rho * vol)
     vy = Momy / (rho * vol)
     P = (Energy/vol - 0.5 * rho * (vx**(2) + vy**(2)) * (gamma - 1))
 
     return rho, vx, vy, P
+
+def getGradident (f, dx):
+
+    """
+      f        is a matrix of the field
+      dx       is the cell size
+      f_dx     is a matrix of derivative of f in the x-direction
+      f_dy     is a matrix of derivative of f in the y-direction
+    """
+
+    # directions for np.roll()
+
+    R = -1 # right
+    L = 1 # left 
+
+    f_dx = (np.roll(f, R, axis = 0) - np.roll(f, L, axis = 0)) / (2 * dx)
+    f_dy = (np.roll(f, R, axis = 1) - np.roll(f, L, axis = 1) ) / (2 * dx)
+
+    return f_dx, f_dy
+
+
+
 
 
 
